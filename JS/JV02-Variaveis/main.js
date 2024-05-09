9338f5a6110f8ee194d4bc2469f6436c8ec9e065
GabrielKameoka <gabrielkameoka@gmail.com>
2024-05-08 21:44:54 -0300

    Aula 02

---
+++ JS/JV02-Variaveis/main.js
@@ -0,0 +1,45 @@
+//Objeto console
+console.log("Olá mundo!")
+
+// Variaveis
+//Quero criar um espaço na memória do computador chamado _felippe e dizer que esse espaço na memória irá receber o valor 8
+var _felippe = 8
+// quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+var _felippe = 2
+
+
+let letsGo = 4
+
+console.log(_felippe)
+
+console.clear()
+
+const PI = 3.1415
+
+console.log(PI)
+
+let NAN = 0/0
+console.log(NAN)
+
+let x = 1/2
+console.log(x)
+
+let corinthians = "Melhor time, palmiras sem mundial"//vaiavel corinthians recebeu uma string
+
+console.log(corinthians)
+
+console.clear()
+
+let IOS = "Instituto da oportunidade social"
+
+console.log(IOS)
+
+console.log("Instituto" + "da" + "oportunidade" + "social") //é possível juntar strings ou variáveis
+
+var num1 = 1
+var num2 = 1
+console.log(num1 == num2)
+
+var num3 = 1
+var num4 = 5
+console.log (num3 == num4)
