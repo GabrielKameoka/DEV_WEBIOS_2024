422f2f49162a30a420e0d1aaf7b3bb78c30e9aff
GabrielKameoka <gabrielkameoka@gmail.com>
2024-05-13 21:25:22 -0300

    exercicio casa

---
+++ JS/JS03-Strings/exercicioCasa/main.js
@@ -0,0 +1,10 @@
+var texto = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log(texto[19])
+console.log(texto.length)
+console.log(texto.toUpperCase())
+console.log(texto.substring(48, 52))
+
+//apenas para ver qual caracter está o dev
+let sla = "Estamos aprendendo JavaScript e seremos futuros "
+console.log(sla.length)
---
+++ JS/JS03-Strings/main.js
@@ -0,0 +1,44 @@
+var string1 = "Os alunos estão "
+var string2 = " reprovados "
+var string3 = "ou aprovados?"
+console.log (string1 + string2 + string3)
+
+//usando tamplate string serve para concatenar string e fazer contas matemática
+console.log(`${string1 + string2 + string3}`)
+
+var num1 = 10
+var num2 = 70
+console.log(`${(num1 + num2)}`)
+
+var parte1 = "amo a "
+var parte2 = "minha "
+var parte3 = "NAMORADAA!!!"
+console.log(`${parte1 + parte2 + parte3}`)
+
+var texto1 = "IOS fez uma excursão na TOTVS com três cabritinhos (alunos)"
+console.log(texto1.charAt(2))
+console.log(texto1[27])
+
+//I O S - T O T V S
+//0 1 2 4 6 7 8 9 10
+//espaços também contam como caracteres
+
+console.clear()
+
+//tamanho
+var valor1 = "namorada"
+console.log(`amo demais a minha ${valor1} ${valor1.length}`)
+
+console.clear()
+
+var texto4 = "aqui está em minusculo"
+var texto5 = "AQUI ESTÁ EM MAIÚSCULO"
+
+
+console.log(`minusculo: ${texto5.toLowerCase()}`)
+console.log(`maiúsculo: ${texto4.toUpperCase()}`)
+
+//substring
+
+var namorada = "a mais bela de todas"
+console.log(namorada.substring(7, 11))
