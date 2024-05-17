891547d224b4c234502d57b4555be209b8f7a218
GabrielKameoka <gabrielkameoka@gmail.com>
2024-05-17 19:18:14 -0300

    Exeríxio Casa

--- JS/JS05-Repeticao/exercicioCasa/main.js
+++ JS/JS05-Repeticao/exercicioCasa/main.js
@@ -6,7 +6,7 @@ if (tabuada === 2 || tabuada === 3) {
   console.log(`Tabuada do número ${tabuada}:`)
 
   for (var numero = 1; numero <= 10; numero++) {
-    console.log(`${tabuada} x ${tabuada} = ${tabuada * numero}`)
+    console.log(`${tabuada} x ${numero} = ${tabuada * numero}`)
   }
 }
 //izi
