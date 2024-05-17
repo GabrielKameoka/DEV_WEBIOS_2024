f2c9652f01dec6fedbd1c9d00ea3f7b0cf8ab50a
GabrielKameoka <gabrielkameoka@gmail.com>
2024-05-16 21:44:32 -0300

    exercicioCasa

--- JS/JS05-Repeticao/exercicioCasa/main.js
+++ JS/JS05-Repeticao/exercicioCasa/main.js
@@ -3,7 +3,7 @@ var tabuada = 2
 
 // pra ver se é 2 ou 3
 if (tabuada === 2 || tabuada === 3) {
-  console.log(`Tabuada do número ${numero}:`)
+  console.log(`Tabuada do número ${tabuada}:`)
 
   for (var numero = 1; numero <= 10; numero++) {
     console.log(`${tabuada} x ${tabuada} = ${tabuada * numero}`)
