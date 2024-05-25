fdd5ac7d463cc4cd3621d63d91f21666ca96bdb7
GabrielKameoka <gabrielkameoka@gmail.com>
2024-05-25 20:23:39 -0300

    Exercício casa

---
+++ JS/JS09-MetodoArray/exercicioCasa/main.js
@@ -0,0 +1,15 @@
+var exercicio1 = [17, 43, 8, 4, 97, 56, 29]
+exercicio1.forEach(verificar)
+function verificar(array){
+    if (array % 2 ===0){
+        console.log(`o numero ${array} é par`)
+    } else{
+        console.log(`o numero ${array} é impar`)
+    }
+}
+
+var exercicio2 = [17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61]
+var filtrar = exercicio2.filter((valor) => {
+    return valor > 20 && valor  <80
+})
+console.log(filtrar)
