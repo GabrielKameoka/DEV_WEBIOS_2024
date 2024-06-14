f5c4144a396789db8d7238c84fbdf8176505a252
GabrielKameoka <gabrielkameoka@gmail.com>
2024-06-14 20:51:39 -0300

    API

--- JS/JS15-API-Parte1/assets/JS/main.js
+++ JS/JS15-API-Parte1/assets/JS/main.js
@@ -1,27 +1,23 @@
 // Função para fazer o request na API 
-function getImage(e) { 
+function getImage(e) {
     // Busca a url da API no HTML 
-    let url_api = document.getElementById('url_api').innerText; 
+    let url_api = document.getElementById('url_api').innerText;
     // console.log(url_api); 
- 
+
     // Função fetch para buscar dados na API 
-    fetch(url_api, { 
-        method: 'GET', 
-    }) 
-        .then((response) => { 
-            return response.json(); 
-        }) 
-        .then((data) => { 
-            // console.log(data); 
-            document.getElementById('json_aqui').innerText = 
-                JSON.stringify(data); 
-            // console.log(data.message) 
- 
-            let imagem = ` 
-                  <img class="rounded img-fluid" src="${data.message}" /> 
-                `; 
- 
+    fetch(url_api, {
+        method: 'GET',
+    })
+        .then((response) => {  // receber o JSON
+            return response.json();
+        })
+        .then((data) => {
+            document.getElementById('json_aqui').innerText =
+                JSON.stringify(data); // para transformar em um objeto
+
+            let imagem = `<img class="rounded img-fluid" src="${data.message}" />`;
+
             // Insere a imagem no elemento HTML com id imagem_aqui 
-            document.querySelector('#imagem_aqui').innerHTML = imagem; 
-        }); 
+            document.querySelector('#imagem_aqui').innerHTML = imagem;
+        });
 }
